e923d5b4ebdff39f09c43ea1b1ef177f1659db00
ankur <ankurp31031@gmail.com>
2021-01-11 02:00:07 +0530

    simple

--- localstorage&SETtimeout/localstorage/admin.js
+++ localstorage&SETtimeout/localstorage/admin.js
@@ -1,4 +1,5 @@
 let display = document.querySelector('#display')
+let table = document.getElementById('table')
 let logoutbtn = document.querySelector('button')
 let naam = document.getElementById('userAdmin') 
 
@@ -16,47 +17,59 @@ window.addEventListener('load',function(){
 })
 
 function displayData(){
-    display.innerHTML = ""
-    let table = document.createElement('table')
-
-    let thead = document.createElement('thead')
-    let tHeadRow = document.createElement('tr')
-
-    let th1 = document.createElement('th')
-    th1.textContent = 'USERNAME'
-
-    let th2  = document.createElement('th')
-    th2.textContent = 'USER-TYPE'
-
-    let th3 = document.createElement('th')
-    th3.textContent = 'LOGIN-TIME'
-
-    tHeadRow.append(th1,th2,th3)
-    thead.append
-
-    let tbody = document.createElement('tbody')
-
-    for(let i=0;i<allData.length;i++){
-        let current = allData[i]
-
-        let tBodyRow = document.createElement('tr')
-
-        let td1 = document.createElement('td')
-        td1.textContent = current['username']
-    
-        let td2  = document.createElement('td')
-        td2.textContent = current['type']
-    
-        let td3 = document.createElement('td')
-        td3.textContent = current['time']
-
-        tBodyRow.append(td1,td2,td3)
-        tbody.append(tBodyRow)
+    table.innerHTML = ""
+    console.log(allData)
+    let output = ""
+    for(i in allData){
+        output += `
+        <tr>
+            <td>${allData[i].username}</td>
+            <td>${allData[i].type}</td>
+            <td>${allData[i].time}</td>
+        </tr>
+    `
     }
-    table.append(thead)
-    table.append(tbody)
+    table.innerHTML = output
+    // let table = document.createElement('table')
 
-    display.append(table)
+    // let thead = document.createElement('thead')
+    // let tHeadRow = document.createElement('tr')
+
+    // let th1 = document.createElement('th')
+    // th1.textContent = 'USERNAME'
+
+    // let th2  = document.createElement('th')
+    // th2.textContent = 'USER-TYPE'
+
+    // let th3 = document.createElement('th')
+    // th3.textContent = 'LOGIN-TIME'
+
+    // tHeadRow.append(th1,th2,th3)
+    // thead.append
+
+    // let tbody = document.createElement('tbody')
+
+    // for(let i=0;i<allData.length;i++){
+    //     let current = allData[i]
+
+    //     let tBodyRow = document.createElement('tr')
+
+    //     let td1 = document.createElement('td')
+    //     td1.textContent = current['username']
+    
+    //     let td2  = document.createElement('td')
+    //     td2.textContent = current['type']
+    
+    //     let td3 = document.createElement('td')
+    //     td3.textContent = current['time']
+
+    //     tBodyRow.append(td1,td2,td3)
+    //     tbody.append(tBodyRow)
+    // }
+    // table.append(thead)
+    // table.append(tbody)
+
+    // display.append(table)
 
 }
 
